75454fa3650336b228d90f5fd4e5e6877b710fa9
Степанищева Анастасия <74156774+Stepanishcheva@users.noreply.github.com>
2021-04-27 19:19:27 +0300

    Update Exception3.php

--- 10/Exception3.php
+++ 10/Exception3.php
@@ -4,9 +4,9 @@ use Exception;
 
 class Exception3 extends Exception
 {
-    public function __construct()
+    public function __construct($mess="Exception 3")
     {
-        parent::__construct("Exception 3");
+        parent::__construct($mess);
     }
 
 }
