737ee0abc74c3bc449757550f3d18e0b2f004be7
Степанищева Анастасия <74156774+Stepanishcheva@users.noreply.github.com>
2021-05-10 20:46:40 +0300

    Create index.php

---
+++ 12/1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<form action="http://localhost:8042/req.php" method="POST">
+    <input type="text" name="str" >
+    <button type="submit" >Отправить
+</form>
+</body>
+</html>
